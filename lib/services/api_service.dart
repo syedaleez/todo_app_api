2a4fe50fa43c91aba6a26927ed0f77beb7e6991c
syedaleez <syedaleezh@gmail.com>
2024-11-26 18:44:29 +0500

    Update api_service.dart

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,45 +1,29 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+// import '../models/item.dart';
 import '../models/todo.dart';
 
 class ApiService {
-  final String baseUrl = 'https://crudcrud.com/api/1bf8db313ba543c9b225c2013dee2b5a';
+  final String baseUrl = 'https://crudcrud.com/api/1bf8db313ba543c9b225c2013dee2b5a/items';
 
-  Future<List<Todo>> fetchTodos() async {
+  Future<List<Item>> getItems() async {
     final response = await http.get(Uri.parse(baseUrl));
     if (response.statusCode == 200) {
-      List jsonResponse = json.decode(response.body);
-      return jsonResponse.map((data) => Todo.fromJson(data)).toList();
+      Iterable jsonResponse = json.decode(response.body);
+      return jsonResponse.map((item) => Item.fromJson(item)).toList();
     } else {
-      throw Exception('Failed to load Todos');
+      throw Exception('Failed to load items');
     }
   }
-
-  // Create a new Todo
-  Future<void> createTodo(Todo todo) async {
-    final response = await http.post(
+  Future<void> addItem(String name) async {
+    await http.post(
       Uri.parse(baseUrl),
       headers: {'Content-Type': 'application/json'},
-      body: json.encode(todo.toJson()),
+      body: json.encode({'name': name}),
     );
-    if (response.statusCode != 201) {
-      throw Exception('Failed to create Todo');
-    }
   }
-  Future<void> updateTodo(String id, Todo todo) async {
-    final response = await http.put(
-      Uri.parse('$baseUrl/$id'),
-      headers: {'Content-Type': 'application/json'},
-      body: json.encode(todo.toJson()),
-    );
-    if (response.statusCode != 200) {
-      throw Exception('Failed to update Todo');
-    }
-  }
-  Future<void> deleteTodo(String id) async {
-    final response = await http.delete(Uri.parse('$baseUrl/$id'));
-    if (response.statusCode != 200) {
-      throw Exception('Failed to delete Todo');
-    }
+
+  Future<void> deleteItem(String id) async {
+    await http.delete(Uri.parse('$baseUrl/$id'));
   }
 }
