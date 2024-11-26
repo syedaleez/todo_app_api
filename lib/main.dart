eb67031d5e7fb918514984aa7467a392aa49676e
syedaleez <syedaleezh@gmail.com>
2024-11-26 18:44:50 +0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,9 @@
 import 'package:flutter/material.dart';
-import 'screens/todo_screen.dart'; 
+import 'screens/todo_screen.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'To-Do App',         
-      theme: ThemeData(
-        primarySwatch: Colors.blue, 
-      ),
-      home: TodoScreen(),           
-    );
-  }
+  runApp(MaterialApp(
+    home: HomeScreen(),
+    debugShowCheckedModeBanner: false,
+  ));
 }
